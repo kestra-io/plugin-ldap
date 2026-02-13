c94829d69429380016f11f24e2252334f1252ac2
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 08:03:19 +0100

    chore: update plugin categories (#76)
    * chore: update plugin categories
    
    * chore: update kestraVersion to 1.2.3
    
    ---------
    
    Co-authored-by: ben8t <pimpaudben@gmail.com>
    Co-authored-by: François Delbrayelle <fdelbrayelle@kestra.io>
    Co-authored-by: Malay Dewangan <66718045+Malaydewangan09@users.noreply.github.com>

--- src/main/java/io/kestra/plugin/ldap/package-info.java
+++ src/main/java/io/kestra/plugin/ldap/package-info.java
@@ -1,7 +1,7 @@
 @PluginSubGroup(
     title = "ldapManager",
     description = "This sub-group of plugins contains tasks for handling ldap jobs.",
-    categories = {PluginSubGroup.PluginCategory.DATABASE, PluginSubGroup.PluginCategory.TRANSFORMATION}
+        categories = { PluginSubGroup.PluginCategory.DATA }
 )
 package io.kestra.plugin.ldap;
 
